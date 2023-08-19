2cda037b2144a623f52d0bd955d17e8c462e19e0
dimitrii <dm.koryakov@gmail.com>
2023-08-19 11:25:32 +0200

    fix minor

---
+++ ezkl_inference/__init__.py
@@ -0,0 +1,1 @@
+from .inference_model_ezkl import *
---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+
+from ezkl_inference import inference_ekzl, preproc_data_features
+
+
+def main():
+    d = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = torch.device(d) if torch.cuda.is_available() else torch.device(d)
+
+    features = preproc_data_features()
+    output = inference_ekzl(features=features, device=device)
+    return output
+
+
+if __name__ == "__main__":
+    main()
